7e530004054ab7c8549cb06854a6ab5efcd7483a
Jonas Gertz <Geertzo@live.se>
2020-02-05 01:10:38 +0100

    almost fixed currency

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-
 $(document).ready(function () {
     //Make a get request as soon as the page loads
     //Add lists with values
-    
+
 
     $.ajax({
         url: '/PHPinlämning3/Inl-mningsuppgift3PHP/api/generelizedApi.php',
@@ -20,20 +19,21 @@ $(document).ready(function () {
 
                 $("#from").append(`<option class="card-text from" value="${name} ${amount} ${currency}">${name} has ${amount} ${currency}`);
                 $("#to").append(`<option class="card-text to" value="${name} ${currency}">${name} has ${amount}`);
-                $("#list").append(`<tr><td class="ID">${id}</td><td class="list-name">${name}</td><td>${accountNumber}</td><td class="list-amount">${amount}</td><td>${currency}</tr>`);
-       
+                $("#list").append(`<tr><td class="ID">${id}</td><td class="list-name">${name}</td><td>${accountNumber}</td><td class="list-amount">${amount}</td><td>${currency}</td</tr>`);
+
             }
-            
+
             for (let i = 0; i < data.result2.length; i++) {
                 let from = data.result2[i].fromPerson;
                 let to = data.result2[i].toPerson;
                 let time = data.result2[i].timeStamp;
+                let currency = data.result[i].currency;
                 let cuttedTime = time.slice(0, 16)
                 let amount = data.result2[i].moneyAmount;
                 let method = data.result2[i].paymentMethod;
                 //console.log(from + " " + to);
 
-                $("#time").append(`<tr><td class='fromT'>${from}</td><td class='toT'>${to}</td><td class='amountT'>${amount} kr</td><td class='timeT'>${cuttedTime}</td><td class='methodT'>${method}</td></tr>`);
+                $("#time").append(`<tr><td class='fromT'>${from}</td><td class='toT'>${to}</td><td class='amountT'>${amount}</td><td class='timeT'>${cuttedTime}</td><td class='methodT'>${method}</td></tr>`);
             }
         },
         error: function (request, status) {
@@ -57,105 +57,118 @@ $(document).ready(function () {
         let toArray = [$("#to").val().split(" ")];
         let toName = toArray[0][0];
         let toCurrency = toArray[0][1];
-        let amountToSend = $(".amount").val();
+        var amountToSend = $(".amount").val();
         let paymentMethod = $('#method').val();
-        
-        //Get the right currency
+
+        //prepare object to send
+        let dataToSend = {};
+
+        //Get the right currency depenings on what the user sends
         $.ajax({
-            url: `https://api.exchangeratesapi.io/latest?symbols=${fromCurrency},${toCurrency}`,
+            url: `https://api.exchangeratesapi.io/latest?base=${fromCurrency}`,
             type: 'GET',
             dataType: 'json',
             cache: false,
             success: function (translate) {
-               
-                for(let i = 0; i < translate.length; i++){
-                    console.log("hej");
+                switch (toCurrency) {
+                    case "EUR":
+                        amountToSend = amountToSend * translate.rates.EUR;
+                        break;
+                    case "USD":
+                        amountToSend = amountToSend * translate.rates.USD;
+                        break;
+                    case "SEK":
+                        amountToSend = amountToSend * translate.rates.SEK;
+                        break;
+
                 }
-       
-               
+                console.log(amountToSend);
+                dataToSend = {
+                    fromName: fromName,
+                    toName: toName,
+                    amount: amountToSend,
+                    paymentMethod: paymentMethod
+                }
+                post(dataToSend);
+
             },
             error: function (request, status) {
                 console.log(request);
                 console.log(status);
             }
-    
+
         });
 
+        function post(dataToSend) {
+            //Parse the amounts to INTS before doing the check
+            let fromAmountAsInt = parseInt(fromAmount);
+            let amountToSendAsInt = parseInt(amountToSend);
+            console.log(fromAmountAsInt + " ska skicka " + amountToSendAsInt);
+            try {
+                if (amountToSendAsInt > fromAmountAsInt) {
+                    throw "The customer does not have enough money";
+                } else {
+                    $.ajax({
+                        type: "POST",
+                        url: "/PHPinlämning3/Inl-mningsuppgift3PHP/api/generelizedApi.php",
+                        data: dataToSend,
+                        cache: false,
+                        success: function (result) {
 
-        let dataToSend = {
-            fromName: fromName,
-            toName: toName,
-            amount : amountToSend,
-            paymentMethod: paymentMethod
-        }
-        
-        //Parse the amounts to INTS before doing the check
-        let fromAmountAsInt = parseInt(fromAmount);
-        let amountToSendAsInt = parseInt(amountToSend);
-        console.log(fromAmountAsInt + " ska skicka " + amountToSendAsInt);
-        try {
-            if (amountToSendAsInt > fromAmountAsInt) {
-                throw "The customer does not have enough money";
-            } else {
-                $.ajax({
-                    type: "POST",
-                    url: "/PHPinlämning3/Inl-mningsuppgift3PHP/api/generelizedApi.php",
-                    data: dataToSend,
-                    cache: false,
-                    success: function (result) {
+                            //once the post is sucessfull, make a GET request for the latest info in the database
+                            //Update the values and the texts
 
-                        //once the post is sucessfull, make a GET request for the latest info in the database
-                        //Update the values and the texts
+                            $.ajax({
+                                url: '/PHPinlämning3/Inl-mningsuppgift3PHP/api/generelizedApi.php',
+                                type: 'GET',
+                                dataType: 'json',
+                                cache: false,
+                                success: function (data) {
+                                    //console.log(data);
+                                    for (let i = 0; i < data.result.length; i++) {
+                                        console.log(data);
+                                        let name = data.result[i].personName;
+                                        let amount = data.result[i].moneyAmount;
+                                        let currency = data.result[i].currency;
+                                        document.getElementsByClassName("from")[i].innerHTML = name + " has " + amount + " " + currency;
+                                        document.getElementsByClassName("to")[i].innerHTML = name + " has " + amount;
+                                        document.getElementsByClassName("list-name")[i].innerHTML = name;
+                                        document.getElementsByClassName("list-amount")[i].innerHTML = amount;
+                                        document.getElementsByClassName("from")[i].value = `${name} ${amount} ${currency}`;
 
-                        $.ajax({
-                            url: '/PHPinlämning3/Inl-mningsuppgift3PHP/api/generelizedApi.php',
-                            type: 'GET',
-                            dataType: 'json',
-                            cache: false,
-                            success: function (data) {
-                                //console.log(data);
-                                for (let i = 0; i < data.result.length; i++) {
-                                    let name = data.result[i].personName;
-                                    let amount = data.result[i].moneyAmount;
-                                    document.getElementsByClassName("from")[i].innerHTML = name + " has " + amount;
-                                    document.getElementsByClassName("to")[i].innerHTML = name + " has " + amount;
-                                    document.getElementsByClassName("list-name")[i].innerHTML = name;
-                                    document.getElementsByClassName("list-amount")[i].innerHTML = amount + " kr";
-                                    document.getElementsByClassName("from")[i].value = `${name} ${amount}`;
-
+                                    }
+                                    for (let i = 0; i < data.result2.length; i++) {
+                                        let from = data.result2[i].fromPerson;
+                                        let to = data.result2[i].toPerson;
+                                        let time = data.result2[i].timeStamp;
+                                        let cuttedTime = time.slice(0, 16)
+                                        let amount = data.result2[i].moneyAmount;
+                                        let paymentMethod = data.result2[i].paymentMethod;
+                                        document.getElementsByClassName("fromT")[i].innerHTML = from;
+                                        document.getElementsByClassName("toT")[i].innerHTML = to;
+                                        document.getElementsByClassName("amountT")[i].innerHTML = amount;
+                                        document.getElementsByClassName("timeT")[i].innerHTML = cuttedTime;
+                                        document.getElementsByClassName("methodT")[i].innerHTML = paymentMethod;
+                                    }
+                                },
+                                error: function (request, status) {
+                                    console.log(request);
+                                    console.log(status);
                                 }
-                                for (let i = 0; i < data.result2.length; i++) {
-                                    let from = data.result2[i].fromPerson;
-                                    let to = data.result2[i].toPerson;
-                                    let time = data.result2[i].timeStamp;
-                                    let amount = data.result2[i].moneyAmount;
-                                    let paymentMethod = data.result2[i].paymentMethod;
-                                    document.getElementsByClassName("fromT")[i].innerHTML = from;
-                                    document.getElementsByClassName("toT")[i].innerHTML = to;
-                                    document.getElementsByClassName("amountT")[i].innerHTML = amount + " kr";
-                                    document.getElementsByClassName("timeT")[i].innerHTML = time;
-                                    document.getElementsByClassName("methodT")[i].innerHTML = paymentMethod;
-                                }
-                            },
-                            error: function (request, status) {
-                                console.log(request);
-                                console.log(status);
-                            }
-                        });
-                    },
-                    error: function (request, status) {
-                        console.log(request);
-                        console.log(status);
-                    }
-                });
+                            });
+                        },
+                        error: function (request, status) {
+                            console.log(request);
+                            console.log(status);
+                        }
+                    });
+                }
+            } catch (err) {
+                console.error(err);
             }
-        } catch (err) {
-            console.error(err);
         }
+
     });
 
-    function myFunction() {
-        var x = document.getElementById("mySelect").value;
-        document.getElementById("demo").innerHTML = "You selected: " + x;
-    }
+
 });
